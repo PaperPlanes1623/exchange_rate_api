ba95bcf57540f9aba4a24ad48dd7ac20a97b1c3f
Dylan Taft <dylbronjames@gmail.com>
2020-04-27 21:14:34 -0600

    added axios to api call

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -15,7 +16,8 @@ app.post("/", function (req, res) {
   let exchangeRate = req.body.exchangeRate;
   let amount = req.body.amount;
 
-  let queryURL = "https://blockchain.info/ticker";
+  let queryURL = "https://api.nomics.com/v1/markets?key=" + process.env.NOMICS_API_KEY;
+  let priceURL = "https://api.nomics.com/v1/prices?key=4329bd0fa529e2db192f7f80b06542a2";
 
   let options = {
     url: queryURL,
@@ -27,21 +29,16 @@ app.post("/", function (req, res) {
     }
   }
 
-  request(options, function (error, response, body) {
-    let data = JSON.parse(body);
-    let price = data.price;
-    console.log(price);
-
-    let currentDate = data.time;
-
-    res.write("<p>Todays Date: " + currentDate + "</p>");
-
-    res.write("<h1>" + amount + " " + yourRate + " is currently worth " + price + " " + exchangeRate + "</h1>");
-
-    res.send();
-  })
-})
+  axios.get(priceURL)
+    .then(response => {
+      res.send(response.data);
+      console.log(response.data);
+    }).catch(err => {
+      console.log("Error fetching from NOMICS", err);
+    });
+});
 
 app.listen(3000, function () {
   console.log("Server running on port 3000");
 })
+
